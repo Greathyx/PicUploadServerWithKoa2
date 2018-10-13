b46b021685334178e483a4770b15532571145838
sherley_huang <834998556@qq.com>
2018-10-13 16:09:41 +0800

    add koa-static and koa2-cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const Koa = require('koa');
 const path = require('path');
+const cors = require('koa2-cors');
 const bodyParser = require('koa-body');
 const session = require('koa-session');
+const staticServe = require('koa-static');
 
 
 //init the database config, run the code only
@@ -14,6 +16,29 @@ db.sequelize.sync({force: true}).then(function () {
 
 
 const app = new Koa();
+
+
+// allow cross domain access
+app.use(cors({
+    origin: function (ctx) {
+        // if (ctx.url === '/test') {
+        //     return false;
+        // }
+        // return '*';
+        return 'http://localhost:8000';
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'Cache-Control', 'X-Requested-With'],
+}));
+
+
+// static file serving
+// http url(e.g.): http://127.0.0.1:3000/upload_db5f2a1f91f5e26ac5958611950153fd.jpg
+let imgPath = path.resolve(__dirname, '../uploads');
+app.use(staticServe(imgPath));
 
 
 // cookie. use session
